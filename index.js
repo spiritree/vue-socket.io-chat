3ad6b6e36bb734537f3219395bff0809d350d451
spiritree <spiritreess@gmail.com>
2017-03-24 18:30:07 +0800

    change name

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.use('/static', express.static('static'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+    console.log(msg);
+  });
 });
 
-http.listen(3000, function() {
-  console.log('listening on *:3000');
+http.listen(port, function() {
+  console.log('listening on *:' + port);
 });
---
+++ static/js/custom.js
@@ -0,0 +1,11 @@
+$(function() {
+  var socket = io();
+  $('form').submit(function() {
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  socket.on('chat message', function(msg) {
+    $('#messages').append($('<li>').text(msg));
+  });
+});
