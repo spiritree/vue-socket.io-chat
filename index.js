fdf52658bbe5056181d0d9167eac084b0585f788
spiritree <spiritreess@gmail.com>
2017-03-30 22:03:34 +0800

    vue

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.use('/static', express.static('static'));
+//读取目录static下的文件
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+//读取根目录下的index.html
 
 io.on('connection', function(socket) {
+  var user = '';
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
     console.log(msg);
--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,22 @@
 $(function() {
+  var vmMessageList = new Vue({
+    el: '#message-list',
+    data: {
+      messages: [a]
+    }
+  });
+
+  var vmUserList = new Vue({
+    el: '#user-list',
+    data: {
+      users: []
+    }
+  });
+
   var socket = io();
   $('form').submit(function() {
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
+    socket.emit('chat message', $('#chattext').val());
+    $('#chattext').val('');
     return false;
-  });
-  socket.on('chat message', function(msg) {
-    $('#messages').append($('<li>').text(msg));
   });
 });
