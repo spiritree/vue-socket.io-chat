8b07213ba60cf950adb6bd4584549bdddf3722b6
spiritree <spiritreess@gmail.com>
2017-09-19 17:49:47 +0800

    update port:8989

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8099,
+    port: 8989,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
